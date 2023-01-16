c8cd478322629cbc27f26fffad8ee290e57054dc
Brandon Li <li.brandon@outlook.com>
2023-01-16 00:15:46 -0600

    Stacking up buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,16 +7,36 @@
 
 import SwiftUI
 
-struct ContentView: View {    
+struct ContentView: View {
     var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
     var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        VStack {
-            Text("Tap the flag of")
-            Text(countries[correctAnswer])
+        ZStack {
+            VStack (spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        // Flag was tapped
+                        
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
+            
+            Color.blue
+                .ignoresSafeArea()
         }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
